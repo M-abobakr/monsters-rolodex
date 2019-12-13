d870eb3ea2922660db2ec2a38fddd5bd1176e1fd
MohamedAboBakr <fares6abobakr@gmail.com>
2019-12-13 09:30:04 +0200

    Updates

---
+++ precache-manifest.fda52d727223c245ea6170e0e5b9d5a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80630b9dfe3a5bc02dca60f33e5db50",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e892cce366383308b7b7",
+    "url": "/monsters-rolodex/static/css/main.8a103a50.chunk.css"
+  },
+  {
+    "revision": "daafcec30377134b6049",
+    "url": "/monsters-rolodex/static/js/2.9471f5f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9471f5f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e892cce366383308b7b7",
+    "url": "/monsters-rolodex/static/js/main.79aec77d.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
