6ef3755c015c8849b383ca4a673b719e9afb1a86
MohamedAboBakr <fares6abobakr@gmail.com>
2019-12-13 09:31:54 +0200

    adding file for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state={
+          monsters:[],
+          searchField:''
+        };
+
+        this.handleChange2 = this.handleChange2.bind(this);
+    }
+
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => this.setState({monsters:users}))
+    }
+    
+    
+
+    // this is a function expression that will be declared the first time this object created 
+    // so (this) keyward represented the class, so don't need a bind in the constructor
+    handleChange = (e)=>{
+        this.setState({searchField: e.target.value});
+    }
+
+    // test handleChange as a regular function with bind in constructor
+    handleChange2(e){
+        this.setState({searchField: e.target.value});
+    }
+
+    render(){
+
+        const { monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(monster=>
+              monster.name.toLowerCase().includes(searchField.toLowerCase())
+          );
+
+        return (
+             
+             
+             
+             <div className='App'>
+                 <h1>Monsters Rolodex</h1>
+                 <SearchBox 
+                    placeholder='search monsters'
+                    handleChange={this.handleChange2}
+                 />
+                 <CardList monsters={filteredMonsters} />
+             </div>
+
+          );
+    }
 }
+
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card.styles.css'
+
+export const Card = (props) =>{
+     
+    
+    return (
+  
+         <div className="card-container">
+
+            <img className="card-img" alt="monster" src={`https://robohash.org/${props.monster.id}/set=set2`} />
+            <h2>{props.monster.name}</h2>
+            <p>{props.monster.email}</p>
+         </div>
+    	);
+
+};
